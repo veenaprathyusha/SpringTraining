dfeaa25c7dfe16aff32ab52a30a627b9be5f10c8
veena prathyusha <veenaprathyusha.b16@iiits.in>
2020-10-16 01:52:18 +0800

    Day 2 Training

---
+++ Day2/AutowiringExample/src/com/training/org/Child.java
@@ -0,0 +1,13 @@
+package com.training.org;
+
+public class Child {
+	
+	public Child(){
+		System.out.println("Default constructor of Child is called");
+	}
+	
+	public void showChildDetails() {
+		System.out.println("Child-> showChildDetails is called");
+	}
+}
+
