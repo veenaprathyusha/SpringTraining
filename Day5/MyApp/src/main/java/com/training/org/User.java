9b4c0b85bd01915f3f480c134e062c08b45ad4e8
veena prathyusha <veenaprathyusha.b16@iiits.in>
2020-10-16 15:26:21 +0800

    Day 5

--- Day5/MyApp/src/main/java/com/training/org/User.java
+++ Day5/MyApp/src/main/java/com/training/org/User.java
@@ -12,5 +12,47 @@ public class User {
 	private boolean verified;
 	private List<Role> roles;
 	private Address address;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public boolean isVerified() {
+		return verified;
+	}
+	public void setVerified(boolean verified) {
+		this.verified = verified;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	
 }
